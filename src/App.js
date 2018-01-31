621be7223a33720bd5cd3b0356cadc861badbad7
colin <dupre.col@gmail.com>
2018-01-31 23:40:40 +0100

    vertion qui marche

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends React.Component {
     constructor() {
         super();
@@ -11,8 +12,13 @@ class App extends React.Component {
         };
         this.handleChange = this.handleChange.bind(this);
     }
+
     handleChange(event) {
-        this.setState({value: event.target.value.toLowerCase()});
+        this.setState({value: event.target.value});
+    }
+
+
+    componentDidMount() {
 
         fetch('seriesList.json',{})
             .then(response => response.json())
@@ -24,29 +30,98 @@ class App extends React.Component {
                 console.log(error);
             })
             .then(function () {
-                // alert("j'ai fait ce que j'ai pu");
+                /*alert("j'ai fait ce que j'ai pu");*/
+            });
+
+        fetch('seriesEpisodesList.json',{})
+            .then(response => response.json())
+            .then(seriesListDepuisFichier => {
+                this.setState({seriesEpisodesList: seriesListDepuisFichier});
+
+            })
+            .catch(function (error) {
+                console.log(error);
             });
 
     }
-    componentDidMount() {
-    }
 
-        render() {
-        return (
+
+    render() {
+            return (
             <div>
+                <li>Tape le nom d'une séries</li>
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
                 <ul>
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                    {this.state.seriesList.length ?
-                        this.state.seriesList.map(item => <li key={item.id}>{item.seriesName}</li>)
-                        : <li>Loading...</li>
+                    {this.state.value != "" ?
+                        this.state.seriesList.filter( a => a.seriesName.toLowerCase().indexOf(this.state.value) > -1)
+                        .map(item => <li key={item.id}>{item.seriesName}
+                            <ul>
+                            {
+                                this.state.seriesEpisodesList.filter(
+                                b => b.serie_id == item.id
+                            ).map(episode => episode.episodes_list.filter(
+                                c => c.episodeName
+                                ).map(name => <li>{name.episodeName}</li>)
+                            )
                     }
-                    <p>{this.state.value} </p>
+                        </ul>
+                        </li>)
+
+
+                    :<p>Vous n'avez rien tapé</p>
+                    }
 
                 </ul>
             </div>
         )
     }
 }
+//     render() {
+//             return (
+//             <div>
+//                 <li>Tape le nom d'une séries</li>
+//                 <input type="text" value={this.state.value} onChange={this.handleChange} />
+//                 <ul>
+//                     <li>{listItems}
+//                         <ul>
+//
+//                                 this.state.seriesEpisodesList.filter(
+//                                 b => b.serie_id == item.id
+//                             ).map(episode => episode.episodes_list.filter(
+//                                 c => c.episodeName
+//                                 ).map(name => <li>{name.episodeName}</li>)
+//                             )
+//                     }
+//                         </ul>
+//                         </li>
+//
+//
+//                     :<p>Vous n'avez rien tapé</p>
+//                     }
+//
+//                 </ul>
+//             </div>
+//         )
+//     }
+// }
 
+// let filtre_name =function(name){
+//     if (name != "" ){
+//         const listItems = numbers.map((name) =>
+//             <ListItem key={name.id.toString()}
+//                       value = {name.seriesName} />
+//         );
+//     }else{
+//     <li>Vous n'avez rien tapé</li>
+//     }
+// };
+// let filtre_liste =function(liste){
+//     this.state.seriesEpisodesList.filter(
+//         b => b.serie_id == item.id
+//     ).map(episode => episode.episodes_list.filter(
+//         c => c.episodeName
+//         ).map(name => <li>{name.episodeName}</li>)
+//     )
+// };
 
 export default App;
